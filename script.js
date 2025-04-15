fbebd798da27a833a96c858a328bbde6ca14d8ac
Sudeep9845 <sudeepsjoshi.dev@gmail.com>
2025-04-15 21:59:36 +0000

    fix : Fix bug and a typo in script.js
    --Fixed propertyName typo error
    --Fixed the declaration of the constants key and audio

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`.audio-box[data-key="${e.keyCode}"]`);
 const allKeys = document.querySelectorAll(".audio-box");
 const allAudio = document.querySelectorAll("audio");
 
-// Add event listener to all keys
 window.addEventListener("keydown", (e) => {
-	playSound(e);
+	playSound(e.keyCode);
 });
-// Add event listener to all mouse clicks
+
 allKeys.forEach((key) => {
 	key.addEventListener("click", () => {
-		playSound();
+		const keyCode = key.getAttribute("data-key");
+		playSound(Number(keyCode)); // convert to number
 	});
 });
 
-// Play sound function
-const playSound = (e) => {
-	if (e.keyCode === "32") {
+const playSound = (keyCode) => {
+	if (keyCode === 32) {
 		pauseAll();
 		return;
 	}
 
-	if (!audio) return; // stop the function if there's no audio
+	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+	const key = document.querySelector(`.audio-box[data-key="${keyCode}"]`);
 
-	pauseAll();
-	audio.currentTime = 0; // rewind to the start
-	audio.play(); // play the audio
+	if (!audio) return;
+
+	pauseAll(); // pause everything first
+
+	audio.currentTime = 0;
+	audio.play();
 	key.classList.add("playing");
 };
-// Pause all sounds and remove the playing class
+
 const pauseAll = () => {
 	allAudio.forEach((audio) => {
-		audio.pause(); // pause all audio
-		audio.currentTime = 0; // rewind to the start
+		audio.pause();
+		audio.currentTime = 0;
 	});
 
 	allKeys.forEach((k) => k.classList.remove("playing"));
 };
-// Remove the playing class after transition ends
-const removeTransition = (e) => {
-	if (e.popertyName !== "transform") return;
+
+const removeTransition = function (e) {
+	if (e.propertyName !== "transform") return;
 	this.classList.remove("playing");
 };
-// Add transition end event listener to all keys
+
 allKeys.forEach((key) =>
 	key.addEventListener("transitionend", removeTransition)
 );
